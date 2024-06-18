d9958e81f10469ad65aa10b5c34f27411d36dedd
erikvw <ew2789@gmail.com>
2024-06-17 20:44:49 -0500

    fake test

--- runtests.py
+++ runtests.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 from edc_test_utils import DefaultTestSettings, func_main
 
-app_name = "edc_protocol_incident"
+app_name = "edc_qareports"
 base_dir = Path(__file__).absolute().parent
 
 project_settings = DefaultTestSettings(
@@ -12,7 +12,12 @@ project_settings = DefaultTestSettings(
     BASE_DIR=base_dir,
     APP_NAME=app_name,
     SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
-    SILENCED_SYSTEM_CHECKS=["sites.E101", "edc_navbar.E002", "edc_navbar.E003"],
+    SILENCED_SYSTEM_CHECKS=[
+        "sites.E101",
+        "edc_navbar.E002",
+        "edc_navbar.E003",
+        "edc_consent.E001",
+    ],
     EDC_AUTH_SKIP_AUTH_UPDATER=True,
     EDC_SITES_REGISTER_DEFAULT=True,
     INSTALLED_APPS=[
@@ -29,23 +34,14 @@ project_settings = DefaultTestSettings(
         "edc_appointment.apps.AppConfig",
         "edc_auth.apps.AppConfig",
         "edc_data_manager.apps.AppConfig",
-        "edc_device.apps.AppConfig",
-        "edc_facility.apps.AppConfig",
         "edc_form_runners.apps.AppConfig",
-        "edc_identifier.apps.AppConfig",
         "edc_lab.apps.AppConfig",
-        "edc_label.apps.AppConfig",
-        "edc_locator.apps.AppConfig",
         "edc_metadata.apps.AppConfig",
         "edc_notification.apps.AppConfig",
-        "edc_offstudy.apps.AppConfig",
         "edc_registration.apps.AppConfig",
         "edc_sites.apps.AppConfig",
-        "edc_timepoint.apps.AppConfig",
-        "edc_visit_schedule.apps.AppConfig",
         "edc_visit_tracking.apps.AppConfig",
-        "edc_prn.apps.AppConfig",
-        "edc_pdf_reports.apps.AppConfig",
+        "edc_visit_schedule.apps.AppConfig",
         "edc_qareports.apps.AppConfig",
         "edc_appconfig.apps.AppConfig",
     ],
