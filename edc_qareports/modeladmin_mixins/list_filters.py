feaa485a6b749838900e76cfb6495b2a5e053dbf
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2024-08-15 14:11:47 +0100

    Allow NoteStatusListFilter to be overridden

--- edc_qareports/modeladmin_mixins/list_filters.py
+++ edc_qareports/modeladmin_mixins/list_filters.py
@@ -1,6 +1,6 @@
 from django.contrib.admin import SimpleListFilter
 from django.db.models import Count, QuerySet
-from edc_constants.constants import CLOSED, NEW, PENDING
+from edc_constants.constants import NEW
 
 from ..choices import NOTE_STATUSES
 
@@ -17,7 +17,7 @@ class NoteStatusListFilter(SimpleListFilter):
 
     def lookups(self, request, model_admin):
         status_dict = {tpl[0]: tpl[1] for tpl in NOTE_STATUSES}
-        names = [(NEW, status_dict[NEW])]
+        names = [(NEW, status_dict.get(NEW, "New"))]
         qs = (
             self.note_model_cls.objects.values("status")
             .order_by("status")
@@ -48,19 +48,11 @@ class NoteStatusListFilter(SimpleListFilter):
                     queryset = queryset.exclude(
                         subject_identifier__in=[obj.get("subject_identifier") for obj in qs]
                     )
-                elif self.value() == PENDING:
+                elif self.value() in [tpl[0] for tpl in NOTE_STATUSES]:
                     qs = self.note_model_cls.objects.values("subject_identifier").filter(
-                        report_model=report_model, status=PENDING
+                        report_model=report_model, status=self.value()
                     )
                     queryset = queryset.filter(
-                        subject_identifier__in=[obj.get("subject_identifier") for obj in qs]
-                    )
-                elif self.value() == CLOSED:
-                    qs = self.note_model_cls.objects.values("subject_identifier").filter(
-                        report_model=report_model,
-                        status=CLOSED,
-                    )
-                    queryset = queryset.exclude(
                         subject_identifier__in=[obj.get("subject_identifier") for obj in qs]
                     )
         return queryset
