3baaaed79d3b283fd157e7c1a899266a3e084130
erikvw <ew2789@gmail.com>
2024-08-21 21:19:48 -0500

    update test

--- edc_qareports/tests/tests/test_qa.py
+++ edc_qareports/tests/tests/test_qa.py
@@ -1,3 +1,5 @@
+import pprint
+
 from django.test import TestCase
 from edc_auth.get_app_codenames import get_app_codenames
 
@@ -14,16 +16,9 @@ class TestQA(TestCase):
             "edc_qareports.change_note",
             "edc_qareports.delete_edcpermissions",
             "edc_qareports.delete_note",
-            "edc_qareports.export_note",
-            "edc_qareports.export_qareportlog",
-            "edc_qareports.export_qareportlogsummary",
-            "edc_qareports.import_note",
             "edc_qareports.view_edcpermissions",
             "edc_qareports.view_note",
             "edc_qareports.view_qareportlog",
             "edc_qareports.view_qareportlogsummary",
-            "edc_qareports.viewallsites_note",
-            "edc_qareports.viewallsites_qareportlog",
-            "edc_qareports.viewallsites_qareportlogsummary",
         ]
         self.assertEqual(codenames, expected_codenames)
