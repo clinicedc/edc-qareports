c70df875f9dcdb59d010d63817e870b9b6a7a0cd
erikvw <ew2789@gmail.com>
2025-06-27 13:56:00 -0500

    fix test, setup and gh-actions config DJ52, py 312,313

--- edc_qareports/tests/models.py
+++ edc_qareports/tests/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.db.models import PROTECT
 from edc_appointment_app.models import SubjectVisit
 from edc_crf.model_mixins import CrfStatusModelMixin
-from edc_lab.model_mixins import CrfWithRequisitionModelMixin, requisition_fk_options
+from edc_lab.model_mixins import (
+    CrfWithRequisitionModelMixin,
+    requisition_fk_options,
+)
 from edc_lab_panel.panels import fbc_panel
 from edc_lab_results.model_mixins import (
     BloodResultsModelMixin,
@@ -17,6 +20,7 @@ from edc_lab_results.model_mixins import (
 )
 from edc_model.models import BaseUuidModel
 from edc_sites.model_mixins import SiteModelMixin
+from edc_utils import get_utcnow
 
 requisition_fk_options.update(to="edc_appointment_app.SubjectRequisition")
 
@@ -40,12 +44,14 @@ class BloodResultsFbc(
 
     subject_visit = models.ForeignKey(SubjectVisit, on_delete=PROTECT)
 
+    report_datetime = models.DateTimeField(default=get_utcnow())
+
     requisition = models.ForeignKey(
         limit_choices_to={"panel__name": fbc_panel.name}, **requisition_fk_options
     )
 
     def get_summary(self):
-        return ""
+        return [], [], []
 
     class Meta(CrfStatusModelMixin.Meta, BaseUuidModel.Meta):
         verbose_name = "Blood Result: FBC"
