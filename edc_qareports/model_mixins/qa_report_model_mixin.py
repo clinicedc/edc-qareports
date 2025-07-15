b49491e8b6075e852e5022252c2ad67cb76cea11
erikvw <ew2789@gmail.com>
2025-07-15 09:49:06 -0500

    remove ref to dashboard for notes admin

--- edc_qareports/model_mixins/qa_report_model_mixin.py
+++ edc_qareports/model_mixins/qa_report_model_mixin.py
@@ -6,6 +6,7 @@ from django.db import OperationalError, connection, models
 from django.db.models import DO_NOTHING, Index
 from edc_utils import get_utcnow
 
+from ..utils import recreate_db_view
 from .qa_reports_permissions import qa_reports_permissions
 
 
@@ -21,47 +22,7 @@ class QaReportModelMixin(models.Model):
 
     @classmethod
     def recreate_db_view(cls, drop: bool | None = None, verbose: bool | None = None):
-        """Manually recreate the database view for models declared
-        with `django_db_views.DBView`.
-
-        Mostly useful when Django raises an OperationalError with a
-        restored DB complaining of 'The user specified as a definer
-        (user@host) does not exist'.
-
-        This does not replace generating a migration with `viewmigration`
-        and running the migration.
-
-        For example:
-            from intecomm_reports.models import Vl
-
-            Vl.recreate_db_view()
-
-        Also, could do something like this (replace details as required):
-            CREATE USER 'edc-effect-live'@'10.131.23.168' IDENTIFIED BY 'xxxxxx';
-            GRANT SELECT ON effect_prod.* to 'edc-effect-live'@'10.131.23.168';
-        """
-        drop = True if drop is None else drop
-        try:
-            sql = cls.view_definition.get(settings.DATABASES["default"]["ENGINE"])  # noqa
-        except AttributeError as e:
-            raise AttributeError(
-                f"Is this model linked to a view? Declare model with `DBView`. Got {e}"
-            )
-        else:
-            sql = sql.replace(";", "")
-            if verbose:
-                print(f"create view {cls._meta.db_table} as {sql};")
-            with connection.cursor() as c:
-                if drop:
-                    try:
-                        c.execute(f"drop view {cls._meta.db_table};")
-                    except OperationalError:
-                        pass
-                c.execute(f"create view {cls._meta.db_table} as {sql};")
-            if verbose:
-                sys.stdout.write(
-                    f"Done. Refreshed DB VIEW `{cls._meta.db_table}` for model {cls}."
-                )
+        recreate_db_view(cls, drop=drop, verbose=verbose)
 
     class Meta:
         abstract = True
